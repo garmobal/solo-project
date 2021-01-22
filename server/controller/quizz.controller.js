8c40c0af9ba2f3d09db9b482e552774a05298f5a
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-22 20:41:30 +0100

    refactor: minor change in quizz.controller

--- server/controller/quizz.controller.js
+++ server/controller/quizz.controller.js
@@ -1,33 +1,30 @@
 const test = require('../models/test.model');
 
 const checkAnswer = async (req, res) => {
-
   const { testid, answer, qid } = req.body;
   let currentTest;
 
   if (req.session) {
     // Fetch whole test from mongoose
     try {
-      currentTest = await test.findOne({_id: testid});
+      currentTest = await test.findOne({ _id: testid });
     } catch (e) {
       res.status(500);
       res.send(e);
     }
     req.session[testid] = currentTest;
-  } 
+  }
   if (req.test) {
     currentTest = req.test;
   }
 
-  // const { qid } = req.params;
-  validateAnswer(currentTest, qid, answer);
-  res.send(validateAnswer(currentTest, qid, answer))
+  res.send(validateAnswer(currentTest, qid, answer));
 };
 
 // Return quizz (no answers)
 const getQuizz = async (req, res) => {
   try {
-    const currentTest = await test.findOne({_id: req.params.id});
+    const currentTest = await test.findOne({ _id: req.params.id });
     const quizz = hideAnswers(currentTest);
     res.send(quizz);
   } catch (e) {
@@ -37,7 +34,7 @@ const getQuizz = async (req, res) => {
 };
 
 // Helper functions
-const hideAnswers = testObj => {
+const hideAnswers = (testObj) => {
   testObj.questions = testObj.questions.map((q) => {
     q.options = q.options.map((opt) => opt.op);
     q.answer = '';
