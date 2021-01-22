1f8c27f3c95ee1727a03759a93ea961a3c2b1d82
Peter French <peterfrench94@gmail.com>
2021-01-22 20:53:59 +0100

    feat: addded post.test test

--- server/__tests__/test.controller.test.js
+++ server/__tests__/test.controller.test.js
@@ -2,13 +2,19 @@ const { app } = require('../app');
 const supertest = require('supertest');
 const request = supertest(app);
 
-// const mockTest = {"questions": [
-//   {options: [{op: "1", correct: false}, {op: "2", correct: false}, {op: "3", correct: true}, {op: "4", correct: false}], "questions": "How many", "answer": "3"},
-//   {options: [{op: "1", correct: true}, {op: "2", correct: false}, {op: "3", correct: false}, {op: "4", correct: false}], "questions": "How much", "answer": "1"},
-//   {options: [{op: "1", correct: false}, {op: "2", correct: true}, {op: "3", correct: false}, {op: "4", correct: false}], "questions": "How to", "answer": "2"},
-//   {options: [{op: "1", correct: false}, {op: "2", correct: false}, {op: "3", correct: false}, {op: "4", correct: true}], "questions": "Howard", "answer": "4"}],
-//   "title": "another test"}
+const mockTest = {
+  assignedto: [],
+  finishedby: [],
+  questions: [
+  {options: [{op: "1", correct: true}, {op: "2", correct: false}, {op: "3", correct: false}, {op: "4", correct: false}], question: "How much", answer: "1"},
+  {options: [{op: "1", correct: false}, {op: "2", correct: false}, {op: "3", correct: true}, {op: "4", correct: false}], question: "How many", answer: "3"},
+  {options: [{op: "1", correct: false}, {op: "2", correct: true}, {op: "3", correct: false}, {op: "4", correct: false}], question: "How to", answer: "2"},
+  {options: [{op: "1", correct: false}, {op: "2", correct: false}, {op: "3", correct: false}, {op: "4", correct: true}], question: "Howard", answer: "4"}],
+  title: "another test",
+  testtype: "Syllable quiz"
+}
 
+//returns 201 but doesnt appear in the DB...
 describe('POST test', () => {
   it('should post a test', async (done) => {
     const response = await request.post('/test').send(mockTest);
