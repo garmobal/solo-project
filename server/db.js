ff2500999a4bb898ad76023a48edcf657df35fc9
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-22 16:54:54 +0100

    feat: installed jest and added a different database when testing. Also created first simple test

--- server/db.js
+++ server/db.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
 
 const db = {};
-const url = 'mongodb://localhost:27017/learntoday'
-mongoose.connect(url, { useNewUrlParser: true,  useUnifiedTopology: true });
+
+if (process.env.JEST_WORKER_ID !== undefined) {
+  const url = 'mongodb://localhost:27017/learntoday-test';
+} else {
+  const url = 'mongodb://localhost:27017/learntoday';
+}
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 db.conn = mongoose;
 
 module.exports = db;
