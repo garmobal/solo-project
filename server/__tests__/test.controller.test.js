a47c1d785096e9421949911ab0d787ba8c99780a
Peter French <peterfrench94@gmail.com>
2021-01-25 11:21:50 +0100

    feat: finished test.controller tests

--- server/__tests__/test.controller.test.js
+++ server/__tests__/test.controller.test.js
@@ -1,11 +1,24 @@
-const { app } = require("../app");
-const supertest = require("supertest");
+const { app } = require('../app');
+const supertest = require('supertest');
 const request = supertest(app);
-const { mockTests } = require("../mocks/mocks");
+const { mockTests } = require('../mocks/mocks');
 
-describe("POST test", () => {
-  it("should post a test if the format is valid", async (done) => {
-    const response = await request.post("/test").send(mockTests.valid);
+let testId;
+
+describe('POST test', () => {
+  it('should not post a test if it doesnt have a title', async (done) => {
+    const response = await request.post('/test').send(mockTests.noTitle);
+    expect(response.status).toBe(500);
+    done();
+  });
+  it('should not post a test if it doesnt have a test type', async (done) => {
+    const response = await request.post('/test').send(mockTests.noTestType);
+    expect(response.status).toBe(500);
+    done();
+  });
+  it('should post a test if the format is valid', async (done) => {
+    const response = await request.post('/test').send(mockTests.valid);
+    testId = response.body._id;
     expect(response.status).toBe(201);
     expect(response.body.assignedto[0]).toEqual(mockTests.valid.assignedto[0]);
     expect(response.body.finishedby).toEqual(mockTests.valid.finishedby);
@@ -16,24 +29,56 @@ describe("POST test", () => {
     expect(response.body.title).toEqual(mockTests.valid.title);
     done();
   });
-  // it("should not post a test if it has less than than 4 questions", async (done) => {
-  // });
 });
 
-// describe("GET tests", () => {
-//   it("should get all tests", async (done) => {
-//     const response = await request.get("/test");
-//     const lastTestPos = response.body.length - 1;
-//     expect(response.status).toBe(200);
-//     expect(response.body[lastTestPos].assignedto[0]).toEqual(
-//       mockTest.assignedto[0]
-//     );
-//     expect(response.body[lastTestPos].finishedby).toEqual(mockTest.finishedby);
-//     expect(response.body[lastTestPos].testtype).toEqual(mockTest.testtype);
-//     expect(response.body[lastTestPos].questions.length).toEqual(
-//       mockTest.questions.length
-//     );
-//     expect(response.body[lastTestPos].title).toEqual(mockTest.title);
-//     done();
-//   });
-// });
+describe('GET a test by ID', () => {
+  it('should not fetch a test if passes an invalid ID', async (done) => {
+    const response = await request.get(`/test/invalidID`);
+    expect(response.status).toBe(500);
+    done();
+  });
+  it('should get the specified test by ID', async (done) => {
+    const response = await request.get(`/test/${testId}`);
+    expect(response.status).toBe(200);
+    expect(response.body.assignedto[0]).toEqual(mockTests.valid.assignedto[0]);
+    expect(response.body.finishedby).toEqual(mockTests.valid.finishedby);
+    expect(response.body.testtype).toEqual(mockTests.valid.testtype);
+    expect(response.body.questions.length).toEqual(
+      mockTests.valid.questions.length
+    );
+    expect(response.body.title).toEqual(mockTests.valid.title);
+    expect(response.body.length).toBe(undefined);
+    done();
+  });
+});
+
+describe('GET tests', () => {
+  it('should get all tests', async (done) => {
+    const response = await request.get('/test');
+    const lastTestPos = response.body.length - 1;
+    expect(response.status).toBe(200);
+    expect(response.body[lastTestPos].assignedto[0]).toEqual(
+      mockTests.valid.assignedto[0]
+    );
+    expect(response.body[lastTestPos].finishedby).toEqual(
+      mockTests.valid.finishedby
+    );
+    expect(response.body[lastTestPos].testtype).toEqual(
+      mockTests.valid.testtype
+    );
+    expect(response.body[lastTestPos].questions.length).toEqual(
+      mockTests.valid.questions.length
+    );
+    expect(response.body[lastTestPos].title).toEqual(mockTests.valid.title);
+    done();
+  });
+});
+
+describe('DELETE test,', () => {
+  it('should delete the test specified by id', async (done) => {
+    const response = await request.delete(`/test/${testId}`);
+    expect(response.status).toBe(204);
+    expect(response.body).toEqual({});
+    done();
+  });
+});
--- server/mocks/mocks.js
+++ server/mocks/mocks.js
@@ -47,44 +47,6 @@ const mockTests = {
     title: 'another test',
     testtype: 'Syllable quiz',
   },
-  threeQuestions: {
-    assignedto: [],
-    finishedby: [],
-    questions: [
-      {
-        options: [
-          { op: '1', correct: true },
-          { op: '2', correct: false },
-          { op: '3', correct: false },
-          { op: '4', correct: false },
-        ],
-        question: 'How much',
-        answer: '1',
-      },
-      {
-        options: [
-          { op: '1', correct: false },
-          { op: '2', correct: false },
-          { op: '3', correct: true },
-          { op: '4', correct: false },
-        ],
-        question: 'How many',
-        answer: '3',
-      },
-      {
-        options: [
-          { op: '1', correct: false },
-          { op: '2', correct: true },
-          { op: '3', correct: false },
-          { op: '4', correct: false },
-        ],
-        question: 'How to',
-        answer: '2',
-      },
-    ],
-    title: 'another test',
-    testtype: 'Syllable quiz',
-  },
   noTitle: {
     assignedto: [],
     finishedby: [],
