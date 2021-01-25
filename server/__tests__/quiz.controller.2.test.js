45bdceafc7f2a6a3d1624efa3a0a400a8ccbe156
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-25 13:00:51 +0100

    feat: added test for the check answer in quiz controller

---
+++ server/__tests__/quiz.controller.2.test.js
@@ -0,0 +1,81 @@
+const { app } = require('../app');
+const supertest = require('supertest');
+const request = supertest(app);
+
+const { mockTests } = require('./../mocks/mocks');
+
+describe('Check Answers', () => {
+  it('should return true if the answer is correct', async (done) => {
+    const createTestRes = await request.post('/test').send(mockTests.valid);
+    let body = {
+      testid: createTestRes.body._id,
+      answer: createTestRes.body.questions[0].answer,
+      qid: createTestRes.body.questions[0]._id,
+      learner: true,
+    };
+    let response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(true);
+    body = {
+      testid: createTestRes.body._id,
+      answer: createTestRes.body.questions[1].answer,
+      qid: createTestRes.body.questions[1]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(true);
+    body = {
+      testid: createTestRes.body._id,
+      answer: createTestRes.body.questions[2].answer,
+      qid: createTestRes.body.questions[2]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(true);
+    body = {
+      testid: createTestRes.body._id,
+      answer: createTestRes.body.questions[3].answer,
+      qid: createTestRes.body.questions[3]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(true);
+    done();
+  });
+
+  it('should return false if the answer is wrong', async (done) => {
+    const createTestRes = await request.post('/test').send(mockTests.valid);
+    let body = {
+      testid: createTestRes.body._id,
+      answer: null,
+      qid: createTestRes.body.questions[0]._id,
+      learner: true,
+    };
+    let response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(false);
+    body = {
+      testid: createTestRes.body._id,
+      answer: null,
+      qid: createTestRes.body.questions[1]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(false);
+    body = {
+      testid: createTestRes.body._id,
+      answer: null,
+      qid: createTestRes.body.questions[2]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(false);
+    body = {
+      testid: createTestRes.body._id,
+      answer: null,
+      qid: createTestRes.body.questions[3]._id,
+      learner: true,
+    };
+    response = await request.post('/quiz').send(body);
+    expect(response.body).toBe(false);
+    done();
+  });
+});
