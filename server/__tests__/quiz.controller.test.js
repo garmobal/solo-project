1434b980df26ab409d6ddfffbd1e1afdbc452c56
Peter French <peterfrench94@gmail.com>
2021-01-25 13:02:08 +0100

    feat: started getQuiz.controller tests

--- server/__tests__/quiz.controller.test.js
+++ server/__tests__/quiz.controller.test.js
@@ -1,7 +1,21 @@
 const { app } = require('../app');
 const supertest = require('supertest');
 const request = supertest(app);
+const { mockTests } = require('../mocks/mocks');
 
-describe('Check Answers', () => {
-  it('should');
+let testID;
+
+beforeAll(async () => {
+  const response = await request.post('/test').send(mockTests.valid);
+  testID = response.body._id;
+});
+
+describe('Get quiz', () => {
+  it('should get the quiz by ID', async (done) => {
+    const response = await request.get(`/test/${testID}`);
+    expect(response.status).toBe(200);
+    expect(response.body._id).toEqual(testID);
+    console.log('response.body ----->', response.body);
+    done();
+  });
 });
