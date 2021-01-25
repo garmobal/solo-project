607045eed8adbf36d31545567062187c8541c195
davidsolsonapuertas <davidsolsonapuertas@gmail.com>
2021-01-25 13:26:00 +0000

    refactor(client): test implementation

--- client/src/components/Teacher/Students/StudentList/StudentCard/StudentCard.tsx
+++ client/src/components/Teacher/Students/StudentList/StudentCard/StudentCard.tsx
@@ -3,43 +3,45 @@ import StudentProgress from '../StudentProgress/StudentProgress';
 import StudentProgressCard from '../StudentProgressCard/StudentProgressCard';
 
 import styles from './StudentCard.module.scss';
+import { Student } from '../../../../../types';
 
-const StudentCard = props => {
+interface IProps {
+  student: Student;
+}
 
+const StudentCard = (props: IProps) => {
   const [showingProgress, setShowingProgress] = useState(false);
 
   const showProgress = () => setShowingProgress(true);
-  
+
   return (
     <React.Fragment>
       <div className={styles.StudentCard}>
         <div className={styles.StudentName}>
-
-          <i className="fas fa-user-alt"/> {props.student.name}
+          <i className="fas fa-user-alt" /> {props.student.name}
         </div>
         <div className={styles.StudentActions}>
           <div className={styles.StudentActionsItem}>
             {props.student.completedtests.length}
-            <i className={`fas fa-check ${styles.Tick}`}></i> 
+            <i className={`fas fa-check ${styles.Tick}`}></i>
           </div>
           <div className={styles.StudentActionsItem}>
             {props.student.pendingtests.length}
-            <i className={`fas fa-circle ${styles.Circle}`}></i> 
+            <i className={`fas fa-circle ${styles.Circle}`}></i>
           </div>
           <div className={styles.StudentActionsItem}>
-            <i 
+            <i
               className={`fas fa-chart-line ${styles.Chart}`}
               onClick={showProgress}
             ></i>
           </div>
-          
         </div>
       </div>
-      <StudentProgressCard 
-        show={showingProgress} 
+      <StudentProgressCard
+        show={showingProgress}
         close={() => setShowingProgress(false)}
       >
-        <StudentProgress student={props.student}/>
+        <StudentProgress student={props.student} />
       </StudentProgressCard>
     </React.Fragment>
   );
