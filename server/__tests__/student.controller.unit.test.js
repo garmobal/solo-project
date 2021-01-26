f0401726930a43669c154b6bb723193477637e1c
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-26 20:21:36 +0100

    feat: added other unit tests for the student controller (one test commented because not working)

--- server/__tests__/student.controller.unit.test.js
+++ server/__tests__/student.controller.unit.test.js
@@ -21,12 +21,19 @@ const mockStudents = [
   },
 ];
 
+const mockStudent = [
+  {
+    name: 'Peter',
+  },
+];
+
 describe('Student controllers unit test', () => {
   const req = {};
   const res = {
     send: jest.fn(() => res).mockName('send'),
     status: jest.fn(() => res).mockName('status'),
   };
+
   describe('get students', () => {
     student.find = jest.fn();
     student.find.mockResolvedValue(mockStudents);
@@ -36,9 +43,37 @@ describe('Student controllers unit test', () => {
       expect(student.find).toHaveBeenCalledTimes(1);
     });
 
-    test('should call res.send with the correct status', async () => {
+    test('should call res.send with the correct argument', async () => {
       await getStudents(req, res);
       expect(res.send).toHaveBeenCalledWith(mockStudents);
     });
+  });
+
+  describe('get student by name', () => {
+    test('student.find should have been called with the name from the body of the request', async () => {
+      student.find = jest.fn();
+      student.find.mockResolvedValue(mockStudent);
+      req.body = {
+        name: 'Peter',
+      };
+      await postStudent(req, res);
+      expect(student.find).toHaveBeenCalledWith(
+        expect.objectContaining({
+          name: req.body.name,
+        })
+      );
+      expect(res.send).toHaveBeenCalledWith(mockStudent[0]);
+    });
+
+    test('res.status should have been called with the correct status ', async () => {
+      await postStudent(req, res);
+      expect(res.status).toHaveBeenCalledWith(201);
+    });
+
+    // test('res.status should have been called with the correct status', async () => {
+    //   student.find.mockImplementation(() => new Error());
+    //   await postStudent(req, res);
+    //   expect(res.status).toHaveBeenCalledWith(500);
+    // });
   });
 });
