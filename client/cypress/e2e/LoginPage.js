ca16cb915ade85561a0745d511b53c15131d80e2
Francesco Fagnani <fagnani.francesco@gmail.com>
2021-01-28 11:01:31 +0100

    feat: finished tests for the login page

--- client/cypress/e2e/LoginPage.js
+++ client/cypress/e2e/LoginPage.js
@@ -1,14 +1,39 @@
-describe('LoginPage', () => {
-  it('can visit the login page', () => {
-    cy.visit('/')
-      .get('[data-testid=logout]')
-      .should('exist')
-      .click()
-      .get('input[name=personname]')
-      .type('vader_loveislove')
-      .get('input[type=submit]')
-      .click()
-      .get('[data-testid=logout]')
-      .should('exist');
+const teacherUsername = 'vader_loveislove';
+const teacherPassword = 'aligator1';
+
+describe('LoginPage test', () => {
+  context('Test inputs', () => {
+    beforeEach(() => {
+      cy.visit('/');
+    });
+
+    it('should type the teacher username in the input[name=personname]', () => {
+      cy.get('[data-testid=logout]')
+        .click()
+        .get('input[name=personname]')
+        .type(teacherUsername)
+        .should('have.value', teacherUsername);
+    });
+
+    it('should type the teacher password in the input[name=password]', () => {
+      cy.get('[data-testid=logout]')
+        .click()
+        .get('input[name=password]')
+        .type(teacherPassword)
+        .should('have.value', teacherPassword);
+    });
+
+    it('should log in as a teacher', () => {
+      cy.get('[data-testid=logout]')
+        .click()
+        .get('input[name=personname]')
+        .type(teacherUsername)
+        .get('input[name=password]')
+        .type(teacherPassword)
+        .get('input[type=submit]')
+        .click()
+        .get('[data-testid=students]')
+        .should('exist');
+    });
   });
 });
